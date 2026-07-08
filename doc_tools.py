3f5192dce2a30de76674e219a78b700dd326dbb3
khanabdullah9 <khanabdullah6292@gmail.com>
2026-07-08 14:20:04 +0530

    program files re-organised

---
+++ doc_tools.py
@@ -0,0 +1,102 @@
+import os
+from typing import Tuple, List
+
+from langchain_core.tools import tool
+from docx import Document
+from docx.shared import Pt
+
+GLOBAL_DOC = Document()
+DOC_PATH = os.path.join("generated","demo.docx")
+
+@tool
+def write_markdown(content: str) -> None:
+    """
+    Writes LLM generated todo list content in a md file
+    :param content:
+    :return:
+    """
+    with open(os.path.join("task","TODO.md"), "w") as md:
+        md.write(content)
+
+@tool
+def write_paragraph(para: str) -> str:
+    """
+    writes paragraph in the doc
+    :param para: paragraph
+    :return: confirmation
+    """
+    global GLOBAL_DOC
+    GLOBAL_DOC.add_paragraph(para)
+    return "paragraph added!"
+
+@tool
+def write_header(header: str) -> str:
+    """
+    writes header in the doc
+    :param header: header
+    :return: confirmation
+    """
+    global GLOBAL_DOC
+    p = GLOBAL_DOC.add_paragraph()
+    run = p.add_run(header)
+    run.font.size = Pt(14)
+    run.bold = True
+    return "header added!"
+
+@tool
+def write_sub_header(sub_header: str) -> str:
+    """
+    writes sub_header in the doc
+    :param sub_header: sub header
+    :return: confirmation
+    """
+    global GLOBAL_DOC
+    p = GLOBAL_DOC.add_paragraph()
+    run = p.add_run(sub_header)
+    run.font.size = Pt(12)
+    run.bold = True
+    return "header added!"
+
+@tool
+def write_bullet_points(points: List[str]) -> str:
+    """
+    writes bullet points in the doc
+    :param points: bullet points
+    :return: confirmation
+    """
+    global GLOBAL_DOC
+    for p in points:
+        GLOBAL_DOC.add_paragraph(p, style = "List Bullet")
+    return "bullet points added!"
+
+@tool
+def write_ordered_list(points: List[str]) -> str:
+    """
+    writes ordered list in the doc
+    :param points: ordered list
+    :return: confirmation
+    """
+    global GLOBAL_DOC
+    for p in points:
+        GLOBAL_DOC.add_paragraph(p, style="List Number")
+    return "bullet points added!"
+
+@tool
+def save_doc() -> str:
+    """
+    Saves the document
+    :return: confirmation
+    """
+    global GLOBAL_DOC
+    GLOBAL_DOC.save(DOC_PATH)
+    return "Doc saved!"
+
+@tool
+def read_markdown() -> str:
+    """
+    Read instructions from a markdown file
+    :return: instructions
+    """
+    with open(os.path.join("task","TODO.md"), "r") as md:
+        content = md.read()
+    return content
